f5490db50ba6bfb42675a7abf194e89bf1c7cd2e
Daeyoon <yoonda5898@gmail.com>
2021-02-03 20:34:03 +0900

    test forms style

--- write/forms.py
+++ write/forms.py
@@ -5,3 +5,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['name', 'title', 'email', 'address', 'productType', 'productNum', 'bodyText', 'picture']
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class' : 'name'})       
