7d9f6a1f2ab28a4dac7d1a737fa41c1da451fb07
Daeyoon <yoonda5898@gmail.com>
2021-02-03 18:32:53 +0900

    modify forms and models

--- write/forms.py
+++ write/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['name', 'title', 'bodyText', 'productType', 'productNum']
+        fields = ['name', 'title', 'email', 'address', 'productType', 'productNum', 'bodyText', 'picture']
