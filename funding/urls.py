f9bb89342c959aa0a09520bc09f2e5cde24b756a
ningpop <craft1933@naver.com>
2021-02-04 02:32:26 +0900

    [ningpop] : apply new model and fix CRUD

--- funding/urls.py
+++ funding/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.funding_list, name="funding_list"),
-    path('create/', views.funding_create, name="funding_create"),
+    path('<int:post_id>/create/', views.funding_create, name="funding_create"),
     path('<int:funding_id>/', views.funding_detail, name="funding_detail"),
     path('<int:funding_id>/update/', views.funding_update, name="funding_update"),
     path('<int:funding_id>/delete/', views.funding_delete, name="funding_delete"),
